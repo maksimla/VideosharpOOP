789776858cb9c4436ed66b8c75e3c164c5b03151
Maksim Lapsinov <maksimla@yandex.ru>
2016-02-06 14:09:13 +0200

    Создание первого класса

--- OOP/Form1.cs
+++ OOP/Form1.cs
@@ -5,21 +5,39 @@ namespace OOP
 {
     public partial class Form1 : Form
     {
+        Line line1;
+        Line line2;
+        Bitmap bmp;
+        Graphics graph;
+        Pen pen;
+
         public Form1()
         {
             InitializeComponent();
+            Init();
             Draw();
+        }
+
+        private void Init()
+        {
+            bmp = new Bitmap(picture.Width, picture.Height);
+            graph = Graphics.FromImage(bmp);
+            pen = new Pen(Color.Blue);
+            line1 = new Line(100, 100, 200, 10);
+            line2 = new Line(200, 10, 300, 100);
         }
 
         private void Draw()
         {
-            Bitmap bmp = new Bitmap(picture.Width, picture.Height);
-            Graphics graph = Graphics.FromImage(bmp);
-            Pen pen = new Pen(Color.Blue);
             graph.DrawRectangle(pen, 100, 100, 200, 200);
-            graph.DrawLine(pen, 100, 100, 200, 10);
-            graph.DrawLine(pen, 200, 10, 300, 100);
+            Draw(line1);
+            Draw(line2);
             picture.Image = bmp;
+        }
+
+        private void Draw(Line line)
+        {
+            graph.DrawLine(pen, line.x1, line.y1, line.x2, line.y2);
         }
     }
 }
