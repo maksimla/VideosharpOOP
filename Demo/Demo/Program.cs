d7cfab84c6dc301e7b0d2607634d24f7e6295a10
Maksim Lapsinov <maksimla@yandex.ru>
2016-02-06 19:52:21 +0200

    Структура vs Класс

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        public struct StructPixel
+        {
+            public int x;
+            public int y;
+
+            public StructPixel(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+
+        public class ClassPixel
+        {
+            public int x;
+            public int y;
+
+            public ClassPixel(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        public class ClassPixel3D : ClassPixel
+        {
+            public int z;
+            public ClassPixel3D(int x, int y,int z):base(x,y)
+            {
+                this.z = z;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DateTime dt;
+            StructPixel sp;
+            ClassPixel cp;
+
+            sp = new StructPixel(10, 20);
+            cp = new ClassPixel(100, 200);
+            MovePixel(ref sp);
+            MovePixel(cp);
+        }
+
+        static void MovePixel(ref StructPixel sp)
+        {
+            sp.x++;
+            sp.y++;
+        }
+
+        static void MovePixel(ClassPixel cp)
+        {
+            cp.x++;
+            cp.y++;
+        }
+    }
+}
