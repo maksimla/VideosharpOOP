2f96657f6ab2298377b60b849717f4c07f7d3e1e
Maksim Lapsinov <maksimla@yandex.ru>
2016-02-19 19:08:10 +0200

    Интерфейс. Правила игры.

---
+++ RunGame/RunGame/GameCatch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace RunGame
+{
+    class GameCatch
+    {
+        public List<IPlayer> Gamers { get; private set; }
+        IPlayer Leader;
+
+        public GameCatch()
+        {
+            Gamers = new List<IPlayer>();
+        }
+
+        public void AddGamer(IPlayer gamer)
+        {
+            Gamers.Add(gamer);
+            SetNewLeader(gamer);
+        }
+
+        public void Step()
+        {
+            RunAll();
+            FindNewLeader();
+        }
+
+        private void FindNewLeader()
+        {
+            foreach (var g in Gamers)
+                if (Leader.IsCatch(g))
+                    SetNewLeader(g);
+        }
+
+        public void RunAll()
+        {
+            foreach (var g in Gamers)
+                g.Run();
+        }
+
+        private void SetNewLeader(IPlayer gamer)
+        {
+            Leader.NoGole();
+            Leader = gamer;
+            Leader.Gole();
+        }
+    }
+}
