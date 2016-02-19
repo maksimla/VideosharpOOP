dc9ca051c2cef9e82d15bb38d6ac021757ee55a2
Maksim Lapsinov <maksimla@yandex.ru>
2016-02-19 21:31:18 +0200

    Интерфейс - Создаём кружок удаляем  неиспользованные using

---
+++ RunGame/RunGame/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace RunGame
+{
+    class Circle
+    {
+        public Point Center { get; private set; }
+        public int Radius { get; private set; }
+        public Color Color { get; private set; }
+
+        public Circle(int x, int y, int r)
+                : this(new Point(x, y), r)
+        {
+        }
+
+        public Circle(Point p, int r)
+        {
+            Center = p;
+            Radius = r;
+            Color = Color.Blue;
+        }
+    }
+}
