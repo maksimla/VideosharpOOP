a4bb9e8150bd520dd3833882efc990029baf7c1c
Maksim Lapsinov <maksimla@yandex.ru>
2016-02-20 10:24:51 +0200

    Интерфейс. Площадка для игры.

---
+++ RunGame/RunGame/Arena.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RunGame
+{
+    class Arena
+    {
+        public static Size Range { get; private set; }
+        private static Random _random = new Random();
+
+        private PictureBox _picture;
+        private Graphics graphics;
+
+        public Arena(PictureBox picture)
+        {
+            _picture = picture;
+            Range = _picture.Size;
+            _picture.Image=new Bitmap(Range.Width,Range.Height);
+            graphics = Graphics.FromImage(_picture.Image);
+            Clear();
+        }
+
+        private void Clear()
+        {
+            graphics.Clear(_picture.BackColor);
+        }
+    }
+}
